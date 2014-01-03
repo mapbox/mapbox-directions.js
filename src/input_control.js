871fe072b12f4463883da2059cd6a404e25b2a94
samanpwbb <samanpwbb@gmail.com>
2014-01-03 17:48:34 -0500

    cleanup form

--- src/input_control.js
+++ src/input_control.js
@@ -28,53 +28,66 @@ module.exports = function (container, directions) {
     var origin = form.append('div')
         .attr('class', 'mapbox-directions-origin-input');
 
-    origin.append('div')
-        .attr('class', 'mapbox-button mapbox-directions-zoom-button')
-        .on('click', function () {
-            if (directions.getOrigin() instanceof L.LatLng) {
-                map.panTo(directions.getOrigin());
-            }
-        })
-        .append('span')
-        .attr('class', 'mapbox-depart-icon mapbox-icon');
+    origin.append('label')
+        .attr('class','mapbox-form-label')
+        .attr('for','mapbox-origin-input')
+        .text('Start location');
 
-    var originInput = origin.append('input')
+    var originContainer = origin.append('div');
+
+        originContainer.append('div')
+            .attr('class', 'mapbox-button mapbox-directions-zoom-button')
+            .on('click', function () {
+                if (directions.getOrigin() instanceof L.LatLng) {
+                    map.panTo(directions.getOrigin());
+                }
+            })
+            .append('span')
+                .attr('class','mapbox-depart-icon mapbox-icon');
+
+    var originInput = originContainer.append('input')
         .attr('type', 'text')
+        .attr('id','mapbox-origin-input')
         .attr('placeholder', 'Start')
         .attr('tabindex', 1);
 
-    var reverse = form.append('div')
-        .attr('class', 'mapbox-directions-reverse-input');
+        form.append('div')
+            .attr('class', 'mapbox-button mapbox-directions-reverse-button mapbox-directions-reverse-input')
+            .on('click', function () {
+                var o = originInput.value,
+                    d = destinationInput.value;
 
-    reverse.append('div')
-        .attr('class', 'mapbox-button mapbox-directions-reverse-button')
-        .on('click', function () {
-            var o = originInput.value,
-                d = destinationInput.value;
+                originInput.property('value', d);
+                destinationInput.property('value', o);
 
-            originInput.property('value', d);
-            destinationInput.property('value', o);
-
-            directions.reverse().query();
-        })
-        .append('span')
-        .attr('class', 'mapbox-reverse-icon mapbox-icon');
+                directions.reverse().query();
+            })
+            .append('span')
+                .attr('class','mapbox-reverse-icon mapbox-icon');
 
     var destination = form.append('div')
         .attr('class', 'mapbox-directions-destination-input');
 
-    destination.append('div')
-        .attr('class', 'mapbox-button mapbox-directions-zoom-button')
-        .on('click', function () {
-            if (directions.getDestination() instanceof L.LatLng) {
-                map.panTo(directions.getDestination());
-            }
-        })
-        .append('span')
-        .attr('class', 'mapbox-arrive-icon mapbox-icon');
+    destination.append('label')
+        .attr('class','mapbox-form-label')
+        .attr('for','mapbox-destination-input')
+        .text('End location');
 
-    var destinationInput = destination.append('input')
+    var destinationContainer = destination.append('div');
+
+        destinationContainer.append('div')
+            .attr('class', 'mapbox-button mapbox-directions-zoom-button')
+            .on('click', function () {
+                if (directions.getDestination() instanceof L.LatLng) {
+                    map.panTo(directions.getDestination());
+                }
+            })
+            .append('span')
+                .attr('class','mapbox-arrive-icon mapbox-icon');
+
+    var destinationInput = destinationContainer.append('input')
         .attr('type', 'text')
+        .attr('id','mapbox-destination-input')
         .attr('placeholder', 'End')
         .attr('tabindex', 1);
 
