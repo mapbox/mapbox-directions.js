c81382b295460398968b3e0067f46ed078b6d4d4
John Firebaugh <john.firebaugh@gmail.com>
2014-01-28 14:37:26 -0800

    Hide X buttons in CSS

--- src/input_control.js
+++ src/input_control.js
@@ -40,17 +40,18 @@ module.exports = function (container, directions) {
         .append('span')
         .attr('class', 'mapbox-directions-icon mapbox-depart-icon');
 
+    var originInput = origin.append('input')
+        .attr('type', 'text')
+        .attr('required', 'required')
+        .attr('id', 'mapbox-directions-origin-input')
+        .attr('placeholder', 'Start');
+
     origin.append('div')
         .attr('class', 'mapbox-directions-icon mapbox-close-icon')
         .attr('title', 'Clear value')
         .on('click', function () {
             directions.setOrigin(undefined);
         });
-
-    var originInput = origin.append('input')
-        .attr('type', 'text')
-        .attr('id', 'mapbox-directions-origin-input')
-        .attr('placeholder', 'Start');
 
     form.append('span')
         .attr('class', 'mapbox-directions-icon mapbox-reverse-icon mapbox-directions-reverse-input')
@@ -72,17 +73,18 @@ module.exports = function (container, directions) {
         .append('span')
         .attr('class', 'mapbox-directions-icon mapbox-arrive-icon');
 
+    var destinationInput = destination.append('input')
+        .attr('type', 'text')
+        .attr('required', 'required')
+        .attr('id', 'mapbox-directions-destination-input')
+        .attr('placeholder', 'End');
+
     destination.append('div')
         .attr('class', 'mapbox-directions-icon mapbox-close-icon')
         .attr('title', 'Clear value')
         .on('click', function () {
             directions.setDestination(undefined);
         });
-
-    var destinationInput = destination.append('input')
-        .attr('type', 'text')
-        .attr('id', 'mapbox-directions-destination-input')
-        .attr('placeholder', 'End');
 
     function format(waypoint) {
         if (!waypoint) {
@@ -98,25 +100,9 @@ module.exports = function (container, directions) {
 
     directions
         .on('origin', function (e) {
-            origin.classed('active', function() {
-                if (e.origin) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-
             originInput.property('value', format(e.origin));
         })
         .on('destination', function (e) {
-            destination.classed('active', function() {
-                if (e.destination) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-
             destinationInput.property('value', format(e.destination));
         })
         .on('load', function (e) {
