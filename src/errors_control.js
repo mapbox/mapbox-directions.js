cd1378df8771171016ae4acc0768c9241ab19fea
John Firebaugh <john.firebaugh@gmail.com>
2014-01-28 14:40:13 -0800

    Clear errors on unload (fixes #31)

--- src/errors_control.js
+++ src/errors_control.js
@@ -4,7 +4,7 @@ var d3 = require('../lib/d3'),
     format = require('./format');
 
 module.exports = function (container, directions) {
-    var control = {}, map, selection = 0;
+    var control = {}, map;
 
     control.addTo = function (_) {
         map = _;
@@ -14,14 +14,13 @@ module.exports = function (container, directions) {
     container = d3.select(L.DomUtil.get(container))
         .classed('mapbox-directions-errors', true);
 
-    directions.on('load', function () {
+    directions.on('load unload', function () {
         container
             .classed('mapbox-error-active', false)
             .html('');
     });
 
     directions.on('error', function (e) {
-
         container
             .classed('mapbox-error-active', true)
             .html('')
@@ -32,7 +31,6 @@ module.exports = function (container, directions) {
         container
             .insert('span', 'span')
             .attr('class', 'mapbox-directions-icon mapbox-error-icon');
-
     });
 
     return control;
